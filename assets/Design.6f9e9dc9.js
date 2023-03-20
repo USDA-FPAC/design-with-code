9b10b5b4f57a1defe732468a41d45bd7b14bc9bb
Casey Hill <bignamehere@gmail.com>
2023-03-20 14:25:45 -0500

    Updates

---
+++ assets/Design.6f9e9dc9.js
@@ -0,0 +1,172 @@
+import{_ as I,d as T,a as C,r as d,u as N,c as g,w as L,k as O,b as B,o as U,e as F,l as p,m as z}from"./index.a5aa1859.js";import{u as G}from"./useNavigation.7fd24a63.js";import{u as R,v}from"./v4.f48cd14a.js";function V(l=null){let o=`<html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <link rel="icon" href="/fpac-design-system/favicon.ico">
+  <link rel="stylesheet" href="https://usda-fsa.github.io/fsa-style/css/fsa-style.min.css" />
+  <link rel="stylesheet" href="https://udsa-fpac.github.io/design-with-code/css/styles.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>FPAC Design System</title>
+  </head>`,t=`<!-- Start Global Nav Component -->
+  <nav class="dwc-overlay">
+    <div class="fsa-nav-global">
+      <div class="fsa-nav-global__bd">
+        <ul class="fsa-nav-global__list" aria-label="Primary Navigation">
+          <li class="fsa-nav-global__list-item">
+            <a class="fsa-nav-global__link fsa-nav-global__link--active" href="/link.html">
+              <span class="fsa-nav-global__text">[item]</span>
+            </a>
+          </li>
+          <li class="fsa-nav-global__list-item">
+            <a class="fsa-nav-global__link" href="/link.html">
+              <span class="fsa-nav-global__text">[item]</span>
+            </a>
+          </li>
+          <li class="fsa-nav-global__list-item">
+            <a class="fsa-nav-global__link" href="/link.html">
+              <span class="fsa-nav-global__text">[item]</span>
+            </a>
+          </li>
+
+          <li class="fsa-nav-global__list-item">
+            <a class="fsa-nav-global__link" href="/link.html">
+              <span class="fsa-nav-global__text">[item]</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- End Global Nav Component -->`,s=`
+  <header>
+    <div class="fsa-tophat">
+      <div class="fsa-tophat__bd">
+        <div class="fsa-tophat__primary">
+          <span class="fsa-tophat__agency">
+            <a class="fsa-tophat__link" href="//usda.gov" title="Link to USDA homepage">
+              <img role="presentation" class="fsa-tophat__agency-logo" src="https://usda-fsa.github.io/fsa-style/img/usda-logo--white.svg" alt="">
+              <abbr class="fsa-tophat__agency-abbr" title="United States Department of Agriculture">USDA</abbr>
+              <span class="fsa-tophat__agency-fullname">United States Department of Agriculture</span>
+            </a>
+          </span>
+        </div>
+        <div class="fsa-tophat__secondary">
+          <span class="fsa-tophat__subagency">
+            Farm Production and Conservation
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="dwc-overlay fsa-header-app">
+      <div class="fsa-header-app__bd">
+        <div class="fsa-header-app__primary">
+          <a class="fsa-header-app__home-link" href="#asdfasfd">
+            <span class="fsa-header-app__app-name">
+              <!-- Your Program Abbreviation and Name -->
+              <abbr class="fsa-header-app__app-abbr">CPS</abbr>
+              <span class="fsa-header-app__app-full">Code Prototype Starter</span>
+              <!-- End Program Addreviation and Name -->
+            </span>
+          </a>
+        </div>
+        <div class="dwc-overlay fsa-header-app__secondary">
+          <span class="fsa-header-app__profile">
+            <span class="fsa-header-app__profile-name">
+              <button HOTSPOT class="fsa-btn fsa-btn--plain fsa-text-size--2 fsa-link:hover--white fsa-link:hover--underline" data-behavior="open-modal" aria-controls="demo-profile-modal" aria-expanded="false" type="button">
+                <svg class="fsa-icon fsa-icon--size-1" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                  <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
+                </svg>
+                Susan Jackson
+              </button>
+            </span>
+            <a href="#link-placeholder" class="fsa-header-app__profile-link">
+              <span class="fsa-level fsa-level--inline fsa-level--gutter-xs">
+                <svg class="fsa-icon fsa-icon--size-1" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z"></path>
+                </svg>
+                <span>Log out</span>
+              </span>
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+
+    ${t}
+  </header>`;return{top:o,headerApp:s,globalNav:t}}function M(l=null){let o=`
+  <main id="main-content" tabindex="-1">
+    <div class="fsa-section">
+      <div class="fsa-section__bd">`,t=`<!-- Stepped Tabs -->
+  <nav class="dwc-overlay" aria-label="Breadcrumbs">
+    <div class="fsa-m-t--l" class="fsa-stepped-tabs">
+      <div class="fsa-stepped-tabs__bd">
+        <ol class="fsa-stepped-tabs__list">
+          <li class="fsa-stepped-tabs__item">
+            <a class="fsa-stepped-tabs__label fsa-stepped-tabs__label--complete" href="/link.html">
+              <span class="fsa-stepped-tabs__text">Lorem</span>
+            </a>
+          </li>
+          <li class="fsa-stepped-tabs__item">
+            <a class="fsa-stepped-tabs__label fsa-stepped-tabs__label--active" href="/link.html" aria-current="step">
+              <span class="fsa-stepped-tabs__text">Header Goes Here</span>
+            </a>
+          </li>
+          <li class="fsa-stepped-tabs__item">
+            <span class="fsa-stepped-tabs__label fsa-stepped-tabs__label--incomplete">
+              <span class="fsa-stepped-tabs__text">Dolor</span>
+            </span>
+          </li>
+          <li class="fsa-stepped-tabs__item">
+            <span class="fsa-stepped-tabs__label fsa-stepped-tabs__label--incomplete">
+              <span class="fsa-stepped-tabs__text">Sit Amet</span>
+            </span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  </nav>
+  <!-- End Stepped Tabs -->`;const s=`<div class="dwc-overlay fsa-m-b--m fsa-level fsa-level--justify-between fsa-level--align-bottom">
+    <div>
+      <h1 class="fsa-m-t--m">Header Goes Here</h1>
+    </div>
+    <div class="fsa-level fsa-hide">
+      <!-- USE AREA FOR PAGE ACTIONS -->
+      <span>
+        <a href="#go-to-settings.html" class="fsa-btn fsa-btn--flat">Settings</a>
+      </span>
+      <span>
+        <button class="fsa-btn fsa-btn--secondary" type="button">Import Thing</button>
+      </span>
+      <!-- USE AREA FOR PAGE ACTIONS -->
+    </div>
+  </div>`;let i=`<!-- Stepped Control -->
+  <div class="dwc-overlay fsa-stepped-control">
+    <div class="fsa-stepped-control__bd">
+      <div class="fsa-stepped-control__message fsa-stepped-control__message--fill" role="status">
+        <strong>1</strong> of <strong>4</strong> | Header Goes Here
+      </div>
+      <div class="fsa-stepped-control__list">
+        <div class="fsa-stepped-control__item fsa-stepped-control__item--pull">
+          <button class="fsa-btn fsa-btn--alt fsa-btn--large fsa-stepped-control__btn" type="button">
+            <span class="fsa-stepped-control__btn-label">Previous</span>
+          </button>
+        </div>
+        <div class="fsa-stepped-control__shim"></div>
+        <div class="fsa-stepped-control__item fsa-stepped-control__item--push">
+          <button onclick="window.top.postMessage('I like Turtles!', '${l}')" class="fsa-btn fsa-btn--primary fsa-btn--large fsa-stepped-control__btn" type="button">
+            <span class="fsa-stepped-control__btn-label">Next</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>`;return{mainTop:o,steppedTabs:t,headerArea:s,steppedControls:i,mainBottom:`
+      </div>
+    </div>
+  </main>`}}const{checkOnProd:$}=R(),m=$()?"https://usda-fpac.github.io":"http://localhost:3000",{top:y,headerApp:w,globalNav:is}=V(m),{mainTop:x,steppedTabs:k,headerArea:S,steppedControls:A,mainBottom:E}=M(m);function j(){let l="",o=!1,t='<div class="fsa-box fsa-text-align--center fsa-border--red fsa-m-t--l" style="height: 200px">Your App Here</div>';const s=`<script>
+  window.addEventListener('load',()=>{
+    console.log('child sending message handshake...');
+    window.top.postMessage('handshake', '${m}');
+  })
+  window.addEventListener('message', (_evt) => {
+    console.log('message from Parent',_evt.data);
+  });
+  <\/script>`,i=e=>{window.addEventListener("message",a=>{a.data==="handshake"&&o!=!0?o=!0:c(a.data,e)})},c=(e,a=null)=>{console.log("Parent received successfully.",e)},r=e=>{let a="";return l=y+w+x+k+S,e.forEach(n=>{let f=n.data;n.type=="paragraph"&&(a+=f.text),n.type=="code"&&(a+=f.text),n.type=="rawHtml"&&(a+=f.html)}),l+=a+A+E+s,l};return{updateSource:(e,a,n=null)=>{if(n)return y+w+x+k+S+t+A+E+s;if(e=="body")return r(a)},listenToFrame:i}}const Y=T(()=>C(()=>import("./BaseHeader.ea2687f2.js"),["assets/BaseHeader.ea2687f2.js","assets/disclaimer.3c578a98.js","assets/index.a5aa1859.js","assets/index.77b96531.css","assets/useNavigation.7fd24a63.js"])),J=T(()=>C(()=>import("./editor.f289ddc5.js"),["assets/editor.f289ddc5.js","assets/index.a5aa1859.js","assets/index.77b96531.css","assets/useNavigation.7fd24a63.js","assets/v4.f48cd14a.js"])),q={components:{baseHeader:Y,editor:J},setup(l,{emit:o}){const t=d("/design-with-code/");N();const{goto:s}=G(),{updateSource:i,listenToFrame:c}=j(),r=d(v()),b=d(v()),e=d(v()),a=g(()=>{}),n=g(()=>{}),f=d(),H=d();let h=d();const u=_=>{h.value=_},P=_=>{u(i("body",_))};return L([a,n],(_,D)=>{f.value=_,H.value=D}),O(()=>{c(e.value),u(i("","",!0))}),{baseUrl:t,goto:s,sourceDoc:h,editorsId:r,canvasId:b,iFrameId:e,updateHtmlCode:P}}},K={id:"main-content",tabindex:"-1"},Q={class:"fds-section fds-section--fullscreen"},W={class:"fds-section__bd"},X={class:"fds-grid fds-grid--no-gutter"},Z={class:"fds-grid__1/1 fds-grid__3/4@l"},ss={class:"dwc-preview fds-box fds-p--none fds-box--bg-100"},as=["id","srcDoc"],es={class:"fds-grid__1/1 fds-grid__1/4@l"};function ts(l,o,t,s,i,c){const r=B("editor");return U(),F("main",K,[p("div",Q,[p("div",W,[p("div",X,[p("div",Z,[p("div",ss,[p("iframe",{id:s.iFrameId,srcDoc:s.sourceDoc,title:"output",sandbox:"allow-scripts",frameborder:"0",width:"100%",height:"100%"},null,8,as)])]),p("div",es,[z(r,{EDITOR_TITLE:"HTML Editor",onEmitOnUpdate:s.updateHtmlCode},null,8,["onEmitOnUpdate"])])])])])])}const ds=I(q,[["render",ts]]);export{ds as default};
