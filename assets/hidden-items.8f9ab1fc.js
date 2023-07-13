f84999ce05504f7893129b05efdb1cbb35f0de99
Casey Hill <bignamehere@gmail.com>
2023-07-13 15:39:34 -0500

    Updates

---
+++ assets/hidden-items.8f9ab1fc.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,e as o}from"./index.2931cd25.js";const s={};function c(n,r,a,_,p,d){return t(),o("div")}const i=e(s,[["render",c]]);export{i as default};
---
+++ assets/useNavigation.e741ea3b.js
@@ -0,0 +1,1 @@
+import{v as i}from"./index.2931cd25.js";function u(){const n=i();return{goto:(o,e="")=>{o.includes("http")?e=="_blank"?window.open(o,e):window.location.href=o:n.push(o)}}}export{u};
